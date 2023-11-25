3011652f5d9b795afe17bd7e81373d437b8f491a
dewisbrown <luismoreno2103@gmail.com>
2023-11-24 19:12:22 -0600

    needs solution

---
+++ 2022/day8.py
@@ -0,0 +1,6 @@
+# read data from text file
+with open('input-text/day8.txt', encoding='utf-8') as file:
+    data = file.readlines()
+
+
+# use two pointers to check rows, figure out how to check columns
