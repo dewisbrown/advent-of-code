d366ea329d676727b445f3236abaf6cf74961a15
dewisbrown <luismoreno2103@gmail.com>
2023-11-24 19:02:51 -0600

    part 1 and 2 solutions use tree structure and recursion

--- 2022/day7.py
+++ 2022/day7.py
@@ -24,6 +24,8 @@ immediately contained by the current directory:
     -dir xyz means that the current directory 
     contains a directory named xyz.
 '''
+res = []
+
 class Tree:
     '''Tree structure.'''
     def __init__(self, node):
@@ -40,7 +42,7 @@ class Tree:
 
 
     def __str__(self):
-        res = self.root.name
+        res = f'{self.root.name} - [{self.root.size}]'
         for child in self.root.children:
             res += f'\n\t{child}'
         return res
@@ -52,6 +54,7 @@ class DirNode:
         self.name = name.strip()
         self.children = []
         self.size = 0
+        self.visited = False
 
         if name != '/':
             self.parent = tree.get_dir()
@@ -65,8 +68,11 @@ class DirNode:
         return self.parent
 
 
+    def visit(self):
+        self.visited = True
+
     def __str__(self) -> str:
-        return f'dir {self.name} - [{self.size}]'
+        return f'{self.name} - [{self.size}]'
 
 
 class FileNode:
@@ -77,7 +83,31 @@ class FileNode:
 
 
     def __str__(self):
-        return f'file {self.name} - [{self.size}]'
+        return f'{self.name} - [{self.size}]'
+
+# dictionary to save node name and size (key: name, value: size)
+nodes = {}
+
+def calculate_size(node: DirNode):
+    size = 0
+
+    for child in node.children:
+        if isinstance(child, DirNode):
+            size += calculate_size(child)
+        else:
+            size += child.size
+    
+    # part 1, add to list of sizes under 100000
+    if size <= 100000:
+        res.append(size)
+    
+    # set size of node
+    node.size = size
+
+    # part 2, add node info to dictionary 
+    # (I guess a list could be used without node names)
+    nodes[node.name] = node.size
+    return size
 
 
 # read data from text file
@@ -115,4 +145,20 @@ if __name__ == '__main__':
                 i += 1
 
     # traverse tree and calculate directory sizes
+    calculate_size(tree.root)
     
+    # part 1
+    ans = 0
+    for num in res:
+        ans += num
+
+    print(f'Part 1 answer: {ans}')
+
+    # part 2, space needed to clear: 2327598
+    minimum = 70000000
+    sortedDict = sorted(nodes.items(), key=lambda x:x[1])
+    for key, value in sortedDict:
+        if value >= 2327598 and value < minimum:
+            minimum = value
+    
+    print(f'Part 2 answer: {minimum}')
