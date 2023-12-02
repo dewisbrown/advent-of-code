1d298fa5cc44ed77fd5161a516c3ca2c460d9b9b
dewisbrown <luismoreno2103@gmail.com>
2023-12-02 08:48:40 -0600

    needs solution

--- 2022/day8.py
+++ 2022/day8.py
@@ -4,3 +4,19 @@ with open('input-text/day8.txt', encoding='utf-8') as file:
 
 
 # use two pointers to check rows, figure out how to check columns
+for line in data:
+    l: int = 0
+    r: int = len(line.strip()) - 1
+
+    left_list: list = []
+    right_list: list = []
+
+    while (l < r):
+        left_height: int = line[l]
+        right_height: int = line[r]
+
+        # something
+        l += 1
+
+        # something
+        r -= 1
