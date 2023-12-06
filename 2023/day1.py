fee350b6e3afb19db2a4cee91efec37b14a01296
Luis Moreno <luismoreno2103@gmail.com>
2023-12-06 15:00:19 -0600

    changed get_num for-loop

--- 2023/day1.py
+++ 2023/day1.py
@@ -3,12 +3,13 @@ with open('input-text/day1.txt', encoding='utf-8') as file:
     data = file.readlines()
 
 
-def get_num(line: str) -> int:
+def get_num(text: str) -> int:
+    '''Extracts 2 digit number from line of text.'''
     digits = []
 
-    for i in range(0, len(line)):
-        if line[i].isdigit():
-            digits.append(line[i])
+    for c in text:
+        if c.isdigit():
+            digits.append(c)
 
     return int(digits[0] + digits[-1])
 
@@ -17,5 +18,5 @@ if __name__ == '__main__':
     nums = []
     for line in data:
         nums.append(get_num(line.strip()))
-    
+
     print(sum(nums))
